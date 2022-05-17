031a449c33f3b7901610eba70bea8144d40542db
Andrew Dutson <Dutsandrew@gmail.com>
2022-05-17 10:38:26 -0600

    restyled add-item menu, priority buttons are functional

--- src/functions/set-priority.js
+++ src/functions/set-priority.js
@@ -2,7 +2,49 @@ export {
     setPriority,
 }
 
-function setPriority(e) {
-    // Priority Handling
-    console.log(e.target.id);
+let displaySelected;
+
+function setPriority(selectedPriority) {
+    'use strict';
+    
+    const priority1 = document.getElementById('priority-button-selector-1');
+    const priority2 = document.getElementById('priority-button-selector-2');
+    const priority3 = document.getElementById('priority-button-selector-3');
+    const priority4 = document.getElementById('priority-button-selector-4');
+
+    if (selectedPriority === 1) {
+        removePreviousSelected();
+        priority1.classList.add('selected-priority-1');
+        displaySelected = 1;
+    } else if (selectedPriority === 2) {
+        removePreviousSelected();
+        priority2.classList.add('selected-priority-2');
+        displaySelected = 2;
+    } else if (selectedPriority === 3) {
+        removePreviousSelected();
+        priority3.classList.add('selected-priority-3');
+        displaySelected = 3;
+    } else if (selectedPriority === 4) {
+        removePreviousSelected();
+        priority4.classList.add('selected-priority-4');
+        displaySelected = 4;
+    }
+}
+
+function removePreviousSelected() {
+
+    const priority1 = document.getElementById('priority-button-selector-1');
+    const priority2 = document.getElementById('priority-button-selector-2');
+    const priority3 = document.getElementById('priority-button-selector-3');
+    const priority4 = document.getElementById('priority-button-selector-4');
+
+    if (displaySelected == 1) {
+        priority1.classList.remove('selected-priority-1');
+    } else if (displaySelected == 2) {
+        priority2.classList.remove('selected-priority-2');
+    } else if (displaySelected == 3) {
+        priority3.classList.remove('selected-priority-3');
+    } else if (displaySelected == 4) {
+        priority4.classList.remove('selected-priority-4');
+    }
 }
--- src/pages/add-item.js
+++ src/pages/add-item.js
@@ -3,6 +3,7 @@ export {
 }
 
 import { setPriority } from '../functions/set-priority';
+import { displayPriority } from '../functions/set-priority';
 import flagSVG from '../svgs/flag.svg';
 
 let addItemFormOpen = false;
@@ -87,10 +88,12 @@ function addItem() {
                             priorityButton1.setAttribute('id', 'priority-button-selector-1');
                             priorityButton1.classList.add('priority-button');
                             priorityButton1.textContent = "Priority 1";
-                            priorityButton1.addEventListener('click', setPriority);
+                            priorityButton1.onclick = function() {
+                                setPriority(1);
+                            }
 
                             const priorityImg1 = document.createElement('img');
-                                priorityImg1.setAttribute('id', 'priority-flag');
+                                priorityImg1.setAttribute('id', 'priority-flag-1');
                                 priorityImg1.classList.add('priority-1');
                                 priorityImg1.src = flagSVG;
 
@@ -100,10 +103,12 @@ function addItem() {
                             priorityButton2.setAttribute('id', 'priority-button-selector-2');
                             priorityButton2.classList.add('priority-button');
                             priorityButton2.textContent = "Priority 2";
-                            priorityButton2.addEventListener('click', setPriority);
+                            priorityButton2.onclick = function() {
+                                setPriority(2);
+                            }
 
                             const priorityImg2 = document.createElement('img');
-                                priorityImg2.setAttribute('id', 'priority-flag');
+                                priorityImg2.setAttribute('id', 'priority-flag-2');
                                 priorityImg2.classList.add('priority-2');
                                 priorityImg2.src = flagSVG;
 
@@ -113,23 +118,27 @@ function addItem() {
                             priorityButton3.setAttribute('id', 'priority-button-selector-3');
                             priorityButton3.classList.add('priority-button');
                             priorityButton3.textContent = "Priority 3";
-                            priorityButton3.addEventListener('click', setPriority);
+                            priorityButton3.onclick = function() {
+                                setPriority(3);
+                            }
 
                             const priorityImg3 = document.createElement('img');
-                                priorityImg3.setAttribute('id', 'priority-flag');
+                                priorityImg3.setAttribute('id', 'priority-flag-3');
                                 priorityImg3.classList.add('priority-3');
                                 priorityImg3.src = flagSVG;
 
                                 priorityButton3.appendChild(priorityImg3);
 
                         const priorityButton4 = document.createElement('div');
-                        priorityButton4.setAttribute('id', 'priority-button-selector-4');
-                        priorityButton4.classList.add('priority-button');
-                        priorityButton4.textContent = "Priority 4";
-                        priorityButton4.addEventListener('click', setPriority);
+                            priorityButton4.setAttribute('id', 'priority-button-selector-4');
+                            priorityButton4.classList.add('priority-button');
+                            priorityButton4.textContent = "Priority 4";
+                            priorityButton4.onclick = function() {
+                                setPriority(4);
+                            }
 
                             const priorityImg4 = document.createElement('img');
-                                priorityImg4.setAttribute('id', 'priority-flag');
+                                priorityImg4.setAttribute('id', 'priority-flag-4');
                                 priorityImg4.classList.add('priority-4');
                                 priorityImg4.src = flagSVG;
 
